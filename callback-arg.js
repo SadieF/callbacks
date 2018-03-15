ef5e6992d596b2c6510769db411e0fea9d0c23d0
Sadie <sadiefreeman@gmail.com>
2018-03-15 18:31:47 +0000

    refactored code with forEach loop

--- callback-arg.js
+++ callback-arg.js
@@ -1,18 +1,16 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      var foundIndex = arr.indexOf("Waldo");
-      found([foundIndex]);   // execute callback
+  arr.forEach(function (ele, i) {
+    if (ele === "Waldo") {
+      found(ele, i); // execute callback
+    }
+  });
 
     }
 
-  }
-}
+function actionWhenFound(ele, i) {
 
-function actionWhenFound([foundIndex]) {
-
-  console.log("Found Waldo at index " + foundIndex);
+  console.log("Found " + ele + " at index " + i + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
