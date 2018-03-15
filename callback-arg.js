bf3c9182e77c1620fbc7ca87cc2275c4f3fb0e2f
Sadie <sadiefreeman@gmail.com>
2018-03-15 18:29:53 +0000

    Print the index of Waldo when found

---
+++ callback-arg.js
@@ -0,0 +1,18 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      var foundIndex = arr.indexOf("Waldo");
+      found([foundIndex]);   // execute callback
+
+    }
+
+  }
+}
+
+function actionWhenFound([foundIndex]) {
+
+  console.log("Found Waldo at index " + foundIndex);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
